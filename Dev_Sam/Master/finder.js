c5a0ba25e21377d903317474faeb61ef4220e7a5
Sam <samtremblay.exe@gmail.com>
2020-04-13 12:59:32 -0400

    Added new master

---
+++ Dev_Sam/Master/finder.js
@@ -0,0 +1,26 @@
+function dataSpawner() {
+//Push values inside of const here
+
+
+
+test.push({id:200,name:'Mahesh',age:35});
+
+
+
+  }
+
+const test = [];
+
+const death = {
+    canada: 19394,
+    usa: 1
+
+  };
+
+
+const recovered = {
+    canada: 140120410450451,
+    usa: 2
+
+
+  };
